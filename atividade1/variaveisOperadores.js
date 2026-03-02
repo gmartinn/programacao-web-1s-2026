63d8b1aa00a6a15ffba2aaa2ed139fc7ba30b21e
gmartinn <guilherme.martin21@Hotmail.com>
2026-03-02 20:15:39 -0300

    atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,23 @@
+// Soma
+let num_A1 = 10;
+let num_B1 = 5;
+let resultadoSoma = num_A1 + num_B1;
+console.log(`Resultado da soma entre ${num_A1} e ${num_B1} é ${resultadoSoma}`);
+
+// Subtração
+let num_A2 = 10;
+let num_B2 = 5;
+let resultadoSubtracao = num_A2 - num_B2;
+console.log(`Resultado da subtração entre ${num_A2} e ${num_B2} é ${resultadoSubtracao}`);
+
+// Multiplicação
+let num_A3 = 10;
+let num_B3 = 5;
+let resultadoMultiplicacao = num_A3 * num_B3;
+console.log(`Resultado da multiplicação entre ${num_A3} e ${num_B3} é ${resultadoMultiplicacao}`);
+
+// Divisão
+let num_A4 = 10;
+let num_B4 = 5;
+let resultadoDivisao = num_A4 / num_B4;
+console.log(`Resultado da divisão entre ${num_A4} e ${num_B4} é ${resultadoDivisao}`);
